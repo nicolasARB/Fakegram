af47b85ad719ed29725467bbeed3de5aa3da4316
Nicolas Daniel <nicoo.bernstein@gmail.com>
2023-03-10 16:13:23 -0300

    merge fix-2

--- src/components/ux-components/Posts/FeedBack.jsx
+++
@@ -1,9 +0,0 @@
-import LikeButton from "./LikeButton";
-
-export default function FeedBack(params) {
-    return <div className="min-w-full h-1/6 bg-slate-700">
-        
-    <LikeButton />
-
-    </div>
-};
--- src/components/ux-components/Profile-stuff/ProfileItems.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProfileItems({title,data}) {
-
-    return<div className="w-full h-auto block">
-        <h2 className="text-lg font-medium block">{title}</h2>
-        <p className="text-base font-bold block">{data}</p>
-    </div>
-    
-};
