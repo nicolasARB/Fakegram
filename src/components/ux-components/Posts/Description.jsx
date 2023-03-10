74b51c8e07a71ee8bf7be94d5a168936ae08a6cb
Nicolas Daniel <nicoo.bernstein@gmail.com>
2023-03-10 16:17:01 -0300

    prepare merge

--- src/components/ux-components/Posts/Description.jsx
+++
@@ -1,9 +0,0 @@
-export default function Description(props) {
-
-    return <div className="min-w-full h-1/5 flex bg-slate-700 text-white px-2"><h1 className="w-full">
-        <a href={props.userUrl} className="text-white font-bold mr-1">{props.userName}</a>
-        {props.userDescription}</h1>
-
-    </div>
-
-};
--- src/components/ux-components/Profile-stuff/ProfilePic.jsx
+++
@@ -1,6 +0,0 @@
-export default function ProfilePic(userInfo) {
-
-    return<img src={userInfo.profilePic} alt={userInfo.userName} className="w-full h-auto rounded-full" />
-    
-    
-};
