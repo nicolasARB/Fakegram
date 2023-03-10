cd9a74cfeddbeeb4bdf87e2ff58263a80756bbc3
Nicoo <nicoo.bernstein@gmail.com>
2023-03-10 10:27:25 -0300

    register backend

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -1,24 +1,72 @@
 import { pool } from '../db.js';
 
-export const getUsers = (req, res) =>{
-res.send("getting users");
+export const getUsers = async (req, res) =>{
+    try{
+
+const [result] = await pool.query("SELECT * FROM user ORDER BY createAt ASC");
+res.json(result)
+} catch(error){
+    return res.status(500).json({message: error.message});
 }
-export const getUser = (req, res) =>{
-    res.send("getting user");
-    } 
-export const createUser = async (req, res) =>{
-    const {username, email, password } = req.body
-const result = await pool.query('INSERT INTO  user(username, password, email) VALUES (?, ?  )', [
-    username,
-    pass,
-    email
-]);
-        res.send("creating user");
-        }          
-export const updateUser = (req, res) =>{
-        res.send("updating user");
+}
+export const getUser = async (req, res) =>{
+    try{
+        const [result] = await pool.query('SELECT * FROM user WHERE id = ?', 
+        [req.params.id]);
+       
+        if(result.length == 0){
+           return res.status(404).json({message: "User not found"});
         }
-export const deleteUser = (req, res) =>{
-            res.send("deleting user");
+       
+        res.json(result[0]);
+    }catch(error){
+        return res.status(500).json({message: error.message});
+    }
+
+    } 
+    
+export const createUser = async (req, res) =>{
+try{
+    const {username, pass, email, description } = req.body
+
+    const [result] = await pool.query(
+        "INSERT INTO user(username, pass, email) VALUES (?, ?, ?)",
+        [username, pass, email]
+      );
+        res.send({
+            id: result.insertId,
+            username,
+            pass,
+            email
+        });
+}catch(error){
+    return res.status(500).json({message: error.message});
+}
+        }          
+export const updateUser = async (req, res) =>{
+try{
+    const {username, pass, email} = req.body;
+
+ const [result] = await pool.query("UPDATE user SET ?", 
+    [req.body, 
+    req.params.id]);
+
+    res.json(result);
+}catch(error){
+    return res.status(500).json({message: error.message});
+}
+        }
+export const deleteUser = async (req, res) =>{
+try{
+    console.log(req.params.id);
+   const [result] = await pool.query("DELETE FROM user WHERE id = ?", 
+   [req.params.id]);
+   if(result.affectedRows === 0){
+    return res.status(404).json({message: "User not found" });
+   }
+            res.json(result);
+}catch(error){
+    return res.status(500).json({message: error.message});
+}
             }
         
